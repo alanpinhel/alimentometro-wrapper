65afb7ffc850e19c1e523404854658fd78baec23
Alan Pinhel <alanpinhel@gmail.com>
2019-01-27 22:26:32 -0200

    importação dentro do teste para funcionar mock

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import Alimento from './alimento';
+import alimentos from '../data/alimentos.json';
 
 export default class AlimentometroWrapper {
   static getAlimentos() {
-    /* eslint-disable global-require */
-    return require('../data/alimentos.json').map(a =>
+    return alimentos.map(a =>
       new Alimento(a.id, a.nome, a.fatorCorrecao, a.indiceConversao));
   }
 }
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -2,12 +2,12 @@ import { expect } from 'chai';
 import mock from 'mock-require';
 
 import Alimento from '../src/alimento';
-import aw from '../src/index';
 
 describe('AlimentometroWrapper', () => {
   it('getAlimentos() deve retornar uma lista de instâncias de Alimento', () => {
     mock('../data/alimentos.json', [{}, {}]);
 
+    const aw = require('../src/index').default;
     expect(aw.getAlimentos()).to.length(2);
     aw.getAlimentos().forEach(alimento => expect(alimento).to.be.an.instanceOf(Alimento));
   });
